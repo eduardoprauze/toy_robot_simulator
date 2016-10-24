5bd5feb39a86ebf1218a3da7680de57f4df3881c
Eduardo Prauze <eduardoprauze@hotmail.com>
2016-10-24 17:21:41 -0400

    refactored specs - defined subjects

--- spec/limits_validator_spec.rb
+++ spec/limits_validator_spec.rb
@@ -3,53 +3,51 @@ require_relative '../lib/limits_validator'
 
 describe LimitsValidator do
 
-  before do
-    @table = Table.new(5)
-    @validator = LimitsValidator.new(@table)
-  end
+  let(:table){Table.new(5)}
+  subject {LimitsValidator.new(table)}
 
   describe "#valid_position?" do
     it "should return true if a position is valid" do
-      result = @validator.valid_position?(1,2)
+      result = subject.valid_position?(1,2)
       expect(result).to be true
     end
     it "should return false if x position is negative" do
-      result = @validator.valid_position?(-1,2)
+      result = subject.valid_position?(-1,2)
       expect(result).to be false
     end
     it "should return false if y position is negative" do
-      result = @validator.valid_position?(1,-2)
+      result = subject.valid_position?(1,-2)
       expect(result).to be false
     end
     it "should return false if x position is out of bounds" do
-      result = @validator.valid_position?(6,2)
+      result = subject.valid_position?(6,2)
       expect(result).to be false
     end
     it "should return false if y position is out of bounds" do
-      result = @validator.valid_position?(6,2)
+      result = subject.valid_position?(6,2)
       expect(result).to be false
     end
   end
 
   describe "#valid_move?" do
     it "should return true if a move is valid" do
-      result = @validator.valid_move?([1,2], 0)
+      result = subject.valid_move?([1,2], 0)
       expect(result).to be true
     end
     it "should return false if the result of a move is a negative x position" do
-      result = @validator.valid_move?([0,0], 270)
+      result = subject.valid_move?([0,0], 270)
       expect(result).to be false
     end
     it "should return false if the result of a move is a negative y position" do
-      result = @validator.valid_move?([0,0], 180)
+      result = subject.valid_move?([0,0], 180)
       expect(result).to be false
     end
     it "should return false if the result of a move is an out of bounds x position" do
-      result = @validator.valid_move?([4,4], 0)
+      result = subject.valid_move?([4,4], 0)
       expect(result).to be false
     end
     it "should return false if the result of a move is an out of bounds y position" do
-      result = @validator.valid_move?([4,4], 90)
+      result = subject.valid_move?([4,4], 90)
       expect(result).to be false
     end
 
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,35 +1,34 @@
 require_relative '../lib/robot'
 
 describe Robot do
-  before do
-    @robot = Robot.new
-  end
+  subject {Robot.new}
+
   describe "initialization" do
     it "should be initialized with default values" do
-      expect(@robot.current_position).to eq([0,0])
-      expect(@robot.direction).to eq(0)
-      expect(@robot.is_placed).to be false
+      expect(subject.current_position).to eq([0,0])
+      expect(subject.direction).to eq(0)
+      expect(subject.is_placed).to be false
     end
   end
 
   describe "#placed?" do
     it "should return the value of is_placed" do
-      expect(@robot.placed?).to be false
+      expect(subject.placed?).to be false
     end
   end
 
   describe "#placed" do
     it "should set is_placed to true " do
-      @robot.place
-      expect(@robot.is_placed).to be true
+      subject.place
+      expect(subject.is_placed).to be true
     end
   end
 
   describe "#report_position" do
     it "should return the position and the direction of the robot" do
-      @robot.current_position = [2,3]
-      @robot.direction = 180
-      expect(@robot.report_position).to eq('2,3,SOUTH')
+      subject.current_position = [2,3]
+      subject.direction = 180
+      expect(subject.report_position).to eq('2,3,SOUTH')
     end
   end
 
